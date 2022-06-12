aef174238ff0f9d30d49d5acab88018ea0783d76
happy-44 <happymittal129@gmail.cvom>
2022-06-12 09:28:50 +0530

    js code

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ let accept_btn = document.querySelectorAll(".btn-primary");
 accept_btn[0].addEventListener("click",function(){
     document.getElementById("cookieNotice").style.display = "none";
 })
+let cookie_consent = getCookie("user_cookie_consent");
+if(cookie_consent != ""){
+    document.getElementById("cookieNotice").style.display = "none";
+}else{
+    document.getElementById("cookieNotice").style.display = "block";
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
