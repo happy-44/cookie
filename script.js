dd769d2fb8208e0b7f953431329aaf1d5bdf6ffa
happy-44 <happymittal129@gmail.cvom>
2022-06-12 05:43:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let accept_btn = document.querySelectorAll(".btn-primary");
+accept_btn[0].addEventListener("click",function(){
+    document.getElementById("cookieNotice").style.display = "none";
+})
